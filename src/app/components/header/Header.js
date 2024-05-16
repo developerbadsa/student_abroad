99f1ba6fc5727a1da8d2fff1c87f6750b0b7ad64
Rahim Badsa <developerbadsa@gmail.com>
2024-05-16 21:33:25 +0600

    create next js app

---
+++ src/app/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='border-b p-4 bg-red-400'>
+      h
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/app/components/homepage/sections/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../header/header';
+
+const Hero = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Hero;
